079c275fd09547a88213ce7331c705b0eb889b33
vu.letruong <vu.letruong@vti.com.vn>
2024-03-08 17:10:29 +0700

    initialization component&pages&axios

---
+++ src/pages/Home.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+      <h1>Welcome</h1>
+      <login></login>
+    </div>
+  </template>
+  
+  <script>
+  import Login from '../pages/Login.vue';
+  
+  export default {
+    components: {
+      Login,
+    },
+  };
+  </script>
+  
